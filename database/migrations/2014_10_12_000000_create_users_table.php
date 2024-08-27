d0e62dfbb0fa6cd1e087446a9e1b8d304041fdcf
Ihfro <jethrojeroso@gmail.com>
2024-08-27 12:25:57 +0800

    db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role',['admin','establshment','user'])->default('user');
+            $table->enum('status',['active','inactive'])->default('active');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
