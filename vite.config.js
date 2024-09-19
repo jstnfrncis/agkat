f64ab446f41f2bd69354ccf5351d38226de8e2f0
jstnfrncis <2021301531@dhvsu.edu.ph>
2024-09-19 12:52:39 +0800

    err

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  
+  plugins: [react()],
+  build: {
+    outDir: 'build', // Ensure this matches the output directory for gh-pages
+  },
+})
